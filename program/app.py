a5301723848be94ad963fa4cbaf496ca50aa1c2f
Almas M <kramal888@gmail.com>
2020-04-29 23:25:41 +0600

    flask get req error fix

--- program/app.py
+++ program/app.py
@@ -7,7 +7,7 @@ import sys
 web_service = Flask(__name__)
 
 @web_service.route('/data', methods=['GET', 'POST'])
-def get(self):
+def mydata():
     #file_url =  "https://www.researchgate.net/publication/51469608/figure/fig1/AS:339688444448772@1457999444671/Chest-X-ray-on-admission-showed-cardiomegaly-with-a-clear-lung.png"
     file_url = request.args.get('file')
     results = subprocess.Popen("python3 main.py " + file_url, shell=True, stdout=subprocess.PIPE).stdout.read()
@@ -15,6 +15,6 @@ def get(self):
     return jr
 
 if __name__ == '__main__':
-	web_service.run(debug=True, host='0.0.0.0', port=80)
+	web_service.run(debug=True, host='127.0.0.1', port=80)
 
 
